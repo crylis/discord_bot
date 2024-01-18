87feae26db0914f996d3458e3999b102e1b6c057
crylis <ask_every.one@hotmail.com>
2024-01-18 10:56:50 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	// Create a new Discord session
+	dg, err := discordgo.New("Bot MTE5NzA4ODIwMjI5MzUxNDI5Mg.GMnCYz.jkdSPZzSQ7gcmK-EIOVx-0r5TL7q51WVyG4kFQ")
+	if err != nil {
+		fmt.Println("Error creating Discord session:", err)
+		return
+	}
+
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		fmt.Println("conent", m.Content)
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "World!")
+		}
+
+	})
+
+	dg.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	// Open a connection to Discord
+	err = dg.Open()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer dg.Close()
+
+	fmt.Println("Bot is now running. Press Ctrl+C to exit.")
+
+	// Wait until a signal is received to gracefully exit the bot
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
